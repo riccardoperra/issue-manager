bb330bc48c5283a2490a1a73516ac0fc18069c9b
riccardoperra <riccardo.perra@icloud.com>
2022-04-21 21:40:27 +0200

    feat: add login route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProjectsBoardComponent } from './features/projects-board/projects-board.component';
+import { AuthGuard } from './shared/auth/auth.guard';
 
-const routes: Routes = [{ path: '', component: ProjectsBoardComponent }];
+const routes: Routes = [
+  { path: '', component: ProjectsBoardComponent, canActivate: [AuthGuard] },
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./features/login/login.module').then((m) => m.LoginModule),
+  },
+];
 
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
       enableTracing: false,
-      // initialNavigation: 'disabled',
+      initialNavigation: 'disabled',
     }),
   ],
   exports: [RouterModule],

f44587817508362f01589ec9177ecbd88156264e
riccardoperra <riccardo.perra@icloud.com>
2022-04-23 20:22:17 +0200

    feat: add LexoRank generation utils

--- src/app/shared/utils/ranking.ts
+++ src/app/shared/utils/ranking.ts
@@ -10,3 +10,19 @@ export const sortByRank = (a: string, b: string): number => {
   const right = parseRank(b);
   return left.compareTo(right);
 };
+
+export const getNextRank = (ranks: string[]): LexoRank => {
+  if (ranks.length === 0) return LexoRank.middle();
+  const lastRank = ranks[ranks.length - 1];
+  return LexoRank.parse(lastRank).genNext();
+};
+
+export const moveRank = (
+  ranks: string[],
+  prevIndex: number,
+  nextIndex: number
+): LexoRank => {
+  const right = ranks[nextIndex];
+  const rank = LexoRank.parse(right);
+  return rank.between(nextIndex > prevIndex ? rank.genNext() : rank.genPrev());
+};

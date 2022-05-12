a66f88aa4ddb4a8dfd5d8ced1652fa957cc72e91
riccardoperra <riccardo.perra@icloud.com>
2022-05-12 08:46:11 +0200

    fix environment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  appwriteProject: '6276291d13e8cb0d8003',
-  appwriteEndpoint: 'http://localhost/v1',
+  appwriteProject: '627959395839464a9ac6',
+  appwriteEndpoint: 'http://165.232.80.41/v1',
 };
